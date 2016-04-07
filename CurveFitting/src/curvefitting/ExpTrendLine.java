af906e9c5a4844bd3e908a266aaa647b4c845809
{yaseminn} <{godot85@gmail.com}>
2016-04-07 12:12:04 +0300

    curve fitting with OLSMultipleLinearRegression

---
+++ CurveFitting/src/curvefitting/ExpTrendLine.java
@@ -0,0 +1,11 @@
+package curvefitting;
+
+public class ExpTrendLine extends OLSTrendLine {
+    @Override
+    protected double[] xVector(double x) {
+        return new double[]{1,x};
+    }
+
+    @Override
+    protected boolean logY() {return true;}
+}
---
+++ CurveFitting/src/curvefitting/PowerTrendLine.java
@@ -0,0 +1,12 @@
+package curvefitting;
+
+public class PowerTrendLine extends OLSTrendLine {
+    @Override
+    protected double[] xVector(double x) {
+        return new double[]{1,Math.log(x)};
+    }
+
+    @Override
+    protected boolean logY() {return true;}
+
+}
